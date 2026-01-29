b421511be7ed2a3b734f055c51ef9d80d44f25ca
yyh04 <continue04@naver.com>
2026-01-29 10:33:30 +0900

    2026-01-29 10:33 Laptop Commit

---
+++ C언어 공부/백준 9935.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+string input;
+string boom;
+
+void Init() {
+	cin >> input;
+	cin >> boom;
+}
+
+void Solve() {
+	for (int i = 0; i < input.size(); i++) {
+
+	}
+}
+
+int main(void) {
+
+	return 0;
+}
