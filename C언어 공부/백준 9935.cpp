ae6bedfa5cc46f43fe2100a9517d41794d3f6c91
yyh04 <continue04@naver.com>
2026-02-02 15:20:30 +0900

    2026-02-02 15:20 Desktop Commit

--- C언어 공부/백준 9935.cpp
+++ C언어 공부/백준 9935.cpp
@@ -1,42 +1,71 @@
 #include <iostream>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
 string input;
 string boom;
-vector<char> tempVec;
-vector<pair<int, int>> memoryVec;
-bool isInProgress = false;
+deque<int> keepQue;
 bool isFRULA = true;
-int levelCnt = 0;
-int curBoomStringIdx = 0;
 
 void Init() {
 	cin >> input;
 	cin >> boom;
 }
 
+void CoughUp() {
+	isFRULA = false;
+
+	while (!keepQue.empty()) {
+		for (int j = 0; j < keepQue.front() + 1; j++) {
+			cout << boom[j];
+		}
+		keepQue.pop_front();
+	}
+}
+
 void Solve() {
 	for (int i = 0; i < input.size(); i++) {
-		if (input[i] == boom[0]) { // 폭발 문자열의 첫 번째 문자를 만난 경우
-			levelCnt++;
-			if (isInProgress) {
-				tempVec.push_back(input[i]);
-				memoryVec.push_back({levelCnt - 1, curBoomStringIdx});
-				curBoomStringIdx = 0;
-			}
-			else if (tempVec.size() == 0){
-				isInProgress = true;
-				tempVec.push_back(input[i]);
-			}
-			else if()
+		if (!keepQue.empty() && input[i] == boom[keepQue.back() + 1]) {
+			keepQue.back()++;
 		}
-		else if()
+		else if (input[i] == boom[0]) {
+			keepQue.push_back(0);
+		}
+		else {
+			isFRULA = false;
+
+			if (keepQue.empty()) {
+				cout << input[i];
+			}
+			else {
+				CoughUp();
+				cout << input[i];
+			}
+		}
+
+		if (!keepQue.empty() && boom.size() == keepQue.back() + 1) {
+			keepQue.pop_back();
+		}
+	}
+
+	if (!keepQue.empty()) {
+		CoughUp();
 	}
 }
 
 int main(void) {
+	ios::sync_with_stdio(false);
+	cout.tie(0);
+	cin.tie(0);
+
+	Init();
+	Solve();
+
+	if (isFRULA) {
+		cout << "FRULA";
+	}
 
 	return 0;
 }
