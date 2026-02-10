80e8b77a76d5b34acddeb52963fecba2112b7f19
yyh04 <continue04@naver.com>
2026-02-10 19:17:05 +0900

    2026-02-10 19:16 Desktop Commit

--- C언어 공부/백준 10830.cpp
+++ C언어 공부/백준 10830.cpp
@@ -6,8 +6,9 @@ using namespace std;
 
 int n, b;
 int level;
-int arr[5][5];
-int origin[5][5];
+int arr[40][5][5]; // index, r, c
+bool visit[40];
+int ans[5][5];
 int temp[5][5];
 
 void Init() {
@@ -18,49 +19,65 @@ void Init() {
     cin >> n >> b;
     int input;
 
+    for (int i = 0; i < n; i++) {
+        arr[0][i][i] = 1;
+        ans[i][i] = 1;
+    }
+
     for (int r = 0; r < n; r++) {
         for (int c = 0; c < n; c++) {
             cin >> input;
-            origin[r][c] = input;
+            arr[1][r][c] = input;
         }
     }
 
-    memcpy(arr, origin, sizeof(origin));
     level = log2(b);
 }
 
-void Square() {
-    memcpy(temp, arr, sizeof(arr));
-    memset(arr, 0, sizeof(arr));
+int* Cal(int _fst, int _snd) {
+    if (visit[_fst + _snd] == true) {
+        return NULL;
+    }
 
     for (int r = 0; r < n; r++) {
         for (int c = 0; c < n; c++) {
             for (int i = 0; i < n; i++) {
-                arr[r][c] += temp[r][i] * temp[i][c];
+                arr[_fst + _snd][r][c] += arr[_fst][r][i] * arr[_snd][i][c];
+            }
+        }
+    }
+
+    visit[_fst + _snd] = true;
+    return 
+}
+
+void Multi(int _idx) {
+    memcpy(temp, ans, sizeof(ans));
+
+    for (int r = 0; r < n; r++) {
+        for (int c = 0; c < n; c++) {
+            for (int i = 0; i < n; i++) {
+                ans[r][c] += temp[r][i] * arr[_idx][i][c];
             }
         }
     }
 }
 
-void Multi() {
-    memcpy(temp, arr, sizeof(arr));
-    memset(arr, 0, sizeof(arr));
-
-    for (int r = 0; r < n; r++) {
-        for (int c = 0; c < n; c++) {
-            for (int i = 0; i < n; i++) {
-                arr[r][c] += temp[r][i] * origin[i][c];
-            }
-        }
+void Pow(int exp) {
+    if (exp == 0) {
+        return;
+    }
+    else if (exp == 1) {
+        return;
+    }
+    else if (exp % 2 == 0) {
+        Cal(Cal(exp / 2, exp / 2), );
     }
 }
 
 void Solve() {
     for (int i = 0; i < level - 1; i++) {
-        Square();
-    }
-    if (b % 2 != 0) {
-        Multi();
+        Cal(pow(2, i), pow(2, i));
     }
 }
 
