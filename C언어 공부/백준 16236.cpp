6c34b1659b840212100d3af08338062da0ca1780
yyh04 <continue04@naver.com>
2026-03-12 13:01:01 +0900

    Laptop Commit

--- C언어 공부/백준 16236.cpp
+++ C언어 공부/백준 16236.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+bool flag = false;
 int n;
 int timer;
 int sharkSize = 2;
@@ -28,47 +29,47 @@ void Init() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < n; j++) {
 			cin >> input;
-			aqua[i][j] = input;
-
 			if (input != 9 && input != 0) {
 				fishVec[input].push_back({ i, j });
+				aqua[i][j] = input;
 			}
 			else if (input == 9) {
 				sharkPos = { i, j };
+				aqua[i][j] = 0;
 			}
+
 		}
 	}
 }
 
-bool IsInRange(int r, int c) {
-	if (0 <= r && r < n && 0 <= c && c < n) {
+bool IsInRange(int _r, int _c) {
+	if (0 <= _r && _r < n && 0 <= _c && _c < n) {
 		return true;
 	}
-
 	return false;
 }
 
 void BFS() { // 먹을 수 있는 물고기가 있는지, 가장 가까운 물고기, 가장 가까운 물고기까지 거리
-	bool visit[20][20];
-	int level = 1;
+	bool visit[20][20] = { false, };
 
-	queue<pair<int, int>> BFSQue;
+	queue<pair<pair<int, int>, int>> BFSQue;
 
-	BFSQue.push(sharkPos);
+	BFSQue.push({ sharkPos, 0 });
 	visit[sharkPos.first][sharkPos.second] = true;
 
 	while (!BFSQue.empty()) {
-		int sharkCurR = BFSQue.front().first;
-		int sharkCurC = BFSQue.front().second;
+		int sharkCurR = BFSQue.front().first.first;
+		int sharkCurC = BFSQue.front().first.second;
+		int searchLevel = BFSQue.front().second;
 		BFSQue.pop();
 
 		for (int i = 0; i < 4; i++) {
 			int sharkNextR = sharkCurR + dr[i];
 			int sharkNextC = sharkCurC + dc[i];
 
-			if (IsInRange(sharkNextR, sharkNextC) && !visit[sharkNextR][sharkNextC] && aqua[sharkNextR][sharkNextC] < sharkSize) {
-				if (aqua[sharkNextR][sharkNextC] != 0) {
-					timer += level;
+			if (IsInRange(sharkNextR, sharkNextC) && !visit[sharkNextR][sharkNextC] && aqua[sharkNextR][sharkNextC] <= sharkSize) {
+				if (aqua[sharkNextR][sharkNextC] != 0 && aqua[sharkNextR][sharkNextC] != sharkSize) {
+					timer += searchLevel;
 					aqua[sharkNextR][sharkNextC] = 0;
 					sharkPos = { sharkNextR, sharkNextC };
 
@@ -77,26 +78,29 @@ void BFS() { // 먹을 수 있는 물고기가 있는지, 가장 가까운 물고기, 가장 가까운 물
 						sharkEatCnt = 0;
 					}
 
+					cout << "물고기 사냥" << '\n';
 					return;
 				}
 
 				visit[sharkNextR][sharkNextC] = true;
-				BFSQue.push({sharkNextR, sharkNextC});
+				BFSQue.push({ {sharkNextR, sharkNextC}, searchLevel + 1 });
 			}
 		}
-
-		level++;
 	}
 	
 
 	cout << timer << '\n';
+	flag = true;
 }
 
 void Solve() {
-	BFS();
+	while (flag == false) {
+		BFS();
+	}
 }
 
 int main(void) {
-
+	Init();
+	Solve();
 	return 0;
 }
